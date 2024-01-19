b00d840f160c47276a2d437a08f180ed7cae0f27
Manik chopra <129728730+manik031@users.noreply.github.com>
2024-01-19 23:17:04 +0530

    Update create_s3_connection.py

--- create_s3_connection.py
+++ create_s3_connection.py
@@ -6,3 +6,7 @@ def create_s3_connection(aws_access_key_id, aws_secret_access_key,region ):
     
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,region_name = region, config=Config(signature_version='s3v4'), endpoint_url='https://user-details-herbex.s3.eu-north-1.amazonaws.com')
     return s3
+aws_access_key_id = 'AKIAVRUVTIGQZ6JSVSZX'
+aws_secret_access_key ='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'
+region ='eu-north-1'
+s3_client = create_s3_connection(aws_access_key_id, aws_secret_access_key, region)
